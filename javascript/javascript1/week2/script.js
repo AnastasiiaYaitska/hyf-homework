b76efc30811103c6d9274b391c0ad957f43d6531
AnastasiiaYaitska <yaitska.anastasiia@gmail.com>
2022-11-28 16:23:20 +0100

    added condition for fullname function

--- javascript/javascript1/week2/script.js
+++ javascript/javascript1/week2/script.js
@@ -3,13 +3,39 @@
 
 // Flight booking fullname function
 
+
+// 1. first part
+
+// function getFullname(firstname, surname) {
+ 
+//     return firstname + " " + surname;
+// };
+
+// console.log(getFullname("Anastasiia", "Yaitska"));
+
+// const fullname1 = "Sascha";
+// const fullname2 = "Nata";
+
+// console.log(getFullname(fullname1, fullname2));
+
+
+// 2. second part
+
+const useFormalName = true;
+const gender = "female"
+
 function getFullname(firstname, surname) {
-    return firstname + " " + surname;
+    if (firstname === "" || firstname === Number) {
+        return "Input field filled out incorrectly";
+    } else if (useFormalName && gender === "female") {
+        return `Lady ${firstname} ${surname}`;
+    } else if (useFormalName && gender === "male") {
+        return `Lord ${firstname} ${surname}`;
+    } else if (useFormalName !== true) {
+       return firstname + " " + surname;
+    } else {
+        return "Try again."
+}
 };
 
-console.log(getFullname("Anastasiia", "Yaitska"));
-
-const fullname1 = "Sascha";
-const fullname2 = "Nata";
-
-console.log(getFullname(fullname1, fullname2));
+ console.log(getFullname("Anastasiia", "Yaitska"));
