d274eea5929537649382cee2a70de974ffacb6d3
AnastasiiaYaitska <yaitska.anastasiia@gmail.com>
2022-11-28 15:42:30 +0100

    added fullname function

---
+++ javascript/javascript1/week2/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+
+// Flight booking fullname function
+
+function getFullname(firstname, surname) {
+    return firstname + " " + surname;
+};
+
+console.log(getFullname("Anastasiia", "Yaitska"));
+
+const fullname1 = "Sascha";
+const fullname2 = "Nata";
+
+console.log(getFullname(fullname1, fullname2));
