a2b86ef7075b53b0021814bc86236580915c0f48
AnastasiiaYaitska <yaitska.anastasiia@gmail.com>
2022-11-29 10:24:38 +0100

    added weather wear function

--- javascript/javascript1/week2/script.js
+++ javascript/javascript1/week2/script.js
@@ -21,21 +21,53 @@
 
 // 2. second part
 
-const useFormalName = true;
-const gender = "female"
+// const useFormalName = true;
+// const gender = "female"
 
-function getFullname(firstname, surname) {
-    if (firstname === "" || firstname === Number) {
-        return "Input field filled out incorrectly";
-    } else if (useFormalName && gender === "female") {
-        return `Lady ${firstname} ${surname}`;
-    } else if (useFormalName && gender === "male") {
-        return `Lord ${firstname} ${surname}`;
-    } else if (useFormalName !== true) {
-       return firstname + " " + surname;
-    } else {
-        return "Try again."
+// function getFullname(firstname, surname) {
+//     if (firstname === "" || firstname === Number) {
+//         return "Input field filled out incorrectly";
+//     } else if (useFormalName && gender === "female") {
+//         return `Lady ${firstname} ${surname}`;
+//     } else if (useFormalName && gender === "male") {
+//         return `Lord ${firstname} ${surname}`;
+//     } else if (useFormalName !== true) {
+//        return firstname + " " + surname;
+//     } else {
+//         return "Try again."
+// }
+// };
+
+//  console.log(getFullname("Anastasiia", "Yaitska"));
+
+
+
+
+// Weather wear
+
+function recommendedWeatherWear(temperature) {
+    if (temperature <= 30 && temperature >= 20) {
+        return "Go to the beach.";
+    } else if (temperature <= 19 && temperature >= 15) {
+        return " T-shirt and shorts. ";
+    } else if (temperature <= 14 && temperature >= 7) {
+        return " Sweeter and trousers. ";
+    } else if (temperature <= 6 && temperature >= 0) {
+        return " Jacket , sweeter and trousers. ";
+    } else if (temperature <= 0 && temperature >= -15) {
+        return "Wnter- jacket, sweeter, trousers. ";
+    } else if (temperature <= -16 && temperature >= -25) {
+        return " Double- Wnter- jacket, double-sweeter, double-trousers.";
+    }
+    // else if (typeof temperature !== Number) {
+    //     return " need a number";
+    // }
+    else {
+        return " I can not help you (";
+    }
 }
-};
 
- console.log(getFullname("Anastasiia", "Yaitska"));
+console.log(recommendedWeatherWear(31)) 
+
+
+
