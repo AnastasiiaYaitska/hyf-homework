439dfae49ead24d777d4467c6df905c1dbf5e55b
AnastasiiaYaitska <yaitska.anastasiia@gmail.com>
2022-12-08 11:33:39 +0100

    added  Series duration of my life

--- javascript/javascript1/week3/script.js
+++ javascript/javascript1/week3/script.js
@@ -43,3 +43,53 @@ console.log(travelTime); // 8 hours and 38 minutes
 
 
 
+// Series duration of my life
+
+
+const seriesDurations = [
+  {
+    title: "Game of thrones",
+    days: 3,
+    hours: 1,
+    minutes: 0,
+  },
+  {
+    title: "Friends",
+    days: 4,
+    hours: 2,
+    minutes: 20,
+  },
+  {
+    title: "How I Met Your Mother",
+    days: 3,
+    hours: 4,
+    minutes: 16,
+    },
+  {
+    title: "Harry Potter",
+    days: 2,
+    hours: 16,
+    minutes: 16,
+    },
+];
+
+function logOutSeriesText(array) {
+    let total = 0;
+    array.forEach(({ title, days, hours, minutes }) => {
+        
+        const fullHours = (days * 24) + hours;
+        const sumOfHoursAndMinutes = (minutes / 60) + fullHours;
+        const convertationToYears = sumOfHoursAndMinutes * 0.0001140794586;
+        const percentageOfLife = (convertationToYears * 100 / 80).toFixed(3);
+       
+        total += Number(percentageOfLife);
+
+        console.log(`${title} took ${percentageOfLife} of my life`);
+       
+
+       
+
+    })
+     return `In total that is ${total.toFixed(2)} % of my life`;
+}
+console.log(logOutSeriesText(seriesDurations));
