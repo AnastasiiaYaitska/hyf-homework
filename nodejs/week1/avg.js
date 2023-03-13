b5513eebef5b351e310ca8c73872b0d3cf10fe58
AnastasiiaYaitska <yaitska.anastasiia@gmail.com>
2023-03-13 12:46:55 +0100

    add file avg

--- nodejs/week1/avg.js
+++ nodejs/week1/avg.js
@@ -2,7 +2,7 @@
 2
 3
 4
-'use strict';
-for (let j = 0; j < process.argv.length; j++) {
-    console.log(j + ' -> ' + (process.argv[j]));
-}
+// 'use strict';
+// for (let j = 0; j < process.argv.length; j++) {
+//     console.log(j + ' -> ' + (process.argv[j]));
+// }
