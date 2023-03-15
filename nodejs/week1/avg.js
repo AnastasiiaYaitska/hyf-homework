a1a427928271fba328e57564ff026afe044c019f
AnastasiiaYaitska <yaitska.anastasiia@gmail.com>
2023-03-15 14:51:13 +0100

    add CLA function

--- nodejs/week1/avg.js
+++ nodejs/week1/avg.js
@@ -1,8 +1,17 @@
+("use strict");
 
-2
-3
-4
-// 'use strict';
-// for (let j = 0; j < process.argv.length; j++) {
-//     console.log(j + ' -> ' + (process.argv[j]));
-// }
+let sum = 0;
+const data = process.argv.slice(2); //return array of command line arguments
+
+if (data.length === 0) {
+  console.log("No arguments provided");
+} else {
+  for (let i = 0; i < data.length; i++) {
+    if (isNaN(data[i])) {
+      console.log("Enter numbers only");
+      return;
+    }
+    sum += +data[i];
+  }
+  console.log("Average: " + (sum / data.length).toFixed(1));
+}
